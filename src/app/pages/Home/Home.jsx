69d7236e658eb9587e062aacba0582ba70b55323
manhdev24 <quadas897@gmail.com>
2025-03-12 20:26:33 +0700

    doc: create image folder and video

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import data from "../../../mock/homepage/homepage.json";
 const Home = () => {
-  return <div>Home</div>;
+  console.log("data: ", data);
+
+  return (
+    <div>
+      data
+      <img src={data[0].sposter} alt="" />
+      <img src={data[0].bposter} alt="" />
+      <video src={data[0].high}></video>
+    </div>
+  );
 };
 
 export default Home;
