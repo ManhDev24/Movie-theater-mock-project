fe7801dc7f3b777fed5b99596286778e1762a6fa
manhdev24 <quadas897@gmail.com>
2025-03-13 09:10:47 +0700

    style: change movie detail ui"

--- src/app/pages/MovieDetail/MovieDetail.jsx
+++ src/app/pages/MovieDetail/MovieDetail.jsx
@@ -1,9 +1,11 @@
 import React, { use } from "react";
-import { Carousel } from "antd";
 import { useParams } from "react-router-dom";
 import moment from "moment";
+import { Tabs, Card, Typography } from "antd";
+const { Title, Text } = Typography;
 
 import "./MovieDetail.scss";
+
 const contentStyle = {
   textAlign: "center",
   color: "white",
@@ -14,15 +16,15 @@ const MovieDetail = () => {
   const { name } = useParams();
   const dates = convertDates(data);
   const times = getTimes(data);
-  console.log("times: ", times);
-
+  const movieData = data[0];
+  console.log("movieData: ", movieData);
   return (
     <div>
       <div
         style={{ backgroundImage: `url(${data[0]?.bposter})` }}
         className="BannerMovie"
       >
-        <div className="popular-movie-slider">
+        <div className="popular-movie-slider mt-5 ">
           <img src={data[0]?.sposter} className="poster" />
           <div className="popular-movie-slider-content">
             <p className="release">{data[0]?.date}</p>
@@ -57,7 +59,7 @@ const MovieDetail = () => {
           </div>
         </div>
       </div>
-      <section>
+      {/* <section>
         <div className="movie-ticket-book">
           <div className="choose-date">
             <p className="heading">choose date:</p>
@@ -81,24 +83,6 @@ const MovieDetail = () => {
           <div className="choose-time">
             <p className="heading">available times:</p>
             <div className="wrapper">
-              {/* <div className="carousel owl-carousel">
-                <div className="card card-1">
-                  <p>3D</p>
-                  <p>14:45</p>
-                </div>
-                <div className="card card-2">
-                  <p>3D</p>
-                  <p>11:45</p>
-                </div>
-                <div className="card card-3">
-                  <p>2D</p>
-                  <p>12:15</p>
-                </div>
-                <div className="card card-4">
-                  <p>3D</p>
-                  <p>13:00</p>
-                </div>
-              </div> */}
               <Carousel style={contentStyle} dots={false} arrows>
                 {dates.map((time, index) => (
                   <div key={index} className="card">
@@ -112,6 +96,51 @@ const MovieDetail = () => {
           </div>
           <button>Buy ticket</button>
         </div>
+      </section> */}
+      <section className="m-10 flex justify-center">
+        <Tabs
+          defaultActiveKey="0"
+          tabPosition="left"
+          className="w-full max-w-5xl mx-auto bg-white rounded-lg shadow-md p-4 flex"
+        >
+          {movieData.cgv_locations.map((cinema, index) => (
+            <Tabs.TabPane
+              key={index}
+              tab={
+                <span className="px-4 py-2 rounded-lg transition-all duration-300 hover:bg-gray-200 block">
+                  {cinema.name}
+                </span>
+              }
+            >
+              {/* Căn giữa thông tin rạp */}
+              <div className="text-center">
+                <Title level={4} className="m-0 text-blue-600">
+                  {cinema.name}
+                </Title>
+                <Text type="secondary" className="block text-gray-500">
+                  {cinema.address}
+                </Text>
+              </div>
+
+              <div className="mt-6 mb-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+                {cinema.showtime.map((show, idx) => (
+                  <Card
+                    key={idx}
+                    className="border p-4 rounded-lg hover:shadow-lg transition-all duration-300 "
+                  >
+                    <Title level={5} className="m-0 text-gray-800">
+                      {show.time}
+                    </Title>
+                    <Text className="text-gray-600">
+                      Ngày: {show.date} - Ghế trống: {show.seats_available}/
+                      {show.total_seats}
+                    </Text>
+                  </Card>
+                ))}
+              </div>
+            </Tabs.TabPane>
+          ))}
+        </Tabs>
       </section>
     </div>
   );
