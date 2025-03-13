7485055fc029e94c9c88e0fd73bb7364b6993cbd
NGUYEN QUANG MINH <quangminh190104@gmail.com>
2025-03-13 12:33:32 +0700

    Feat: LoadingStartup.jxs

--- src/app/routes/MainRoutes.jsx
+++ src/app/routes/MainRoutes.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../layouts/Header";
 import PageNotFound from "../layouts/PageNotFound";
 import Footer from "../layouts/Footer/Footer";
 import Home from "../pages/Home";
 import MovieDetail from "../pages/MovieDetail/MovieDetail";
+import LoadingStartup from "../components/LoadingStartup";
+
 const MainRoutes = () => {
+  const [loadingStartup, setLoadingStartup] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadingStartup(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/detail/:name" element={<MovieDetail />} />
-      </Routes>
-      <Footer />
+      {loadingStartup ? (
+        <LoadingStartup />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/detail/:name" element={<MovieDetail />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 };
