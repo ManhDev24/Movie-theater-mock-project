152bf0b8723fa4d719ba917909861f9a3e9d6867
NguynnVy <nguyenvyscorpio3112004@gmail.com>
2025-03-14 22:57:08 +0700

    feat: update banner, card

--- src/app/pages/Home/partials/Cards/TitleCards.jsx
+++ src/app/pages/Home/partials/Cards/TitleCards.jsx
@@ -1,13 +1,96 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./TitleCards.scss";
-import data from "../../../../../mock/homepage/homepage.json";
-
+import { sMovie } from "../../../../store/Store";
+import coming from "../../../../../mock/comming.json"; 
 const TitleCards = ({ title }) => {
+  const movies = sMovie.use();
+  const [movieData, setMovieData] = useState([]);
+
   const cardsRef = useRef();
   const navigate = useNavigate();
   const [showLeftArrow, setShowLeftArrow] = useState(false);
   const [showRightArrow, setShowRightArrow] = useState(true);
+
+  useEffect(() => {
+    // Handle different data sources based on title prop
+    if (title === "PHIM SẮP CHIẾU" && coming?.data?.result) {
+      // Use the coming.json data for upcoming movies
+      const currentDate = new Date();
+      const upcomingMovies = coming.data.result
+        .filter(movie => {
+          const startDate = new Date(movie.startDate);
+          return startDate > currentDate;
+        })
+        .map(movie => ({
+          id: movie.id,
+          name: movie.name,
+          imageLandscape: movie.imageLandscape,
+          imagePortrait: movie.imagePortrait,
+          startDate: movie.startDate,
+        }));
+      setMovieData(upcomingMovies);
+    } 
+    
+    else if(title === "PHIM ĐANG CHIẾU" && Object.keys(movies).length > 0) {
+      const firstKey = Object.keys(movies)[0]; 
+      const cinemas = movies[firstKey]?.cinemas;
+
+      if (cinemas) {
+        const allMovies = [];
+        const seenMovieIds = new Set(); 
+
+        Object.keys(cinemas).forEach((cinemaId) => {
+          const cinema = cinemas[cinemaId];
+          if (cinema.movies) {
+            Object.keys(cinema.movies).forEach((movieId) => {
+              if (!seenMovieIds.has(movieId)) {
+                seenMovieIds.add(movieId);
+                const movie = cinema.movies[movieId];
+                allMovies.push({
+                  id: movieId,
+                  name: movie.name,
+                  imageLandscape: movie.imageLandscape,
+                  imagePortrait: movie.imagePortrait,
+                });
+              }
+            });
+          }
+        });
+
+        setMovieData(allMovies);
+      }
+    }
+    else if (movies && Object.keys(movies).length > 0) {
+      const firstKey = Object.keys(movies)[0]; 
+      const cinemas = movies[firstKey]?.cinemas;
+
+      if (cinemas) {
+        const allMovies = [];
+        const seenMovieIds = new Set(); 
+
+        Object.keys(cinemas).forEach((cinemaId) => {
+          const cinema = cinemas[cinemaId];
+          if (cinema.movies) {
+            Object.keys(cinema.movies).forEach((movieId) => {
+              if (!seenMovieIds.has(movieId)) {
+                seenMovieIds.add(movieId);
+                const movie = cinema.movies[movieId];
+                allMovies.push({
+                  id: movieId,
+                  name: movie.name,
+                  imageLandscape: movie.imageLandscape,
+                  imagePortrait: movie.imagePortrait,
+                });
+              }
+            });
+          }
+        });
+
+        setMovieData(allMovies);
+      }
+    }
+  }, [movies, title]);
 
   const scrollRight = () => {
     if (cardsRef.current) {
@@ -48,14 +131,15 @@ const TitleCards = ({ title }) => {
           </button>
         )}
         <div className="card-list" ref={cardsRef}>
-          {data.map((card, index) => (
+          {movieData.map((movie, index) => (
             <div
               className="card"
-              key={index}
-              onClick={() => navigate(`/movie/${card.name}`, { state: { movie: card } })}
+              key={`${movie.id}-${index}`}
+              onClick={() =>
+                navigate(`/movie/${movie.name}`, { state: { movie } })
+              }
             >
-              <img src={card.sposter} alt={card.name} />
-              <p>{card.name}</p>
+              <img src={movie.imagePortrait} alt={movie.name} />
             </div>
           ))}
         </div>
