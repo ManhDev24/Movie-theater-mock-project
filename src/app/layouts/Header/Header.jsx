f33d4d20bc3014caebfbc001f0016e11a6a932cf
NguynnVy <nguyenvyscorpio3112004@gmail.com>
2025-03-15 01:13:27 +0700

    feat: fix loop and UI

--- src/app/layouts/Header/Header.jsx
+++ src/app/layouts/Header/Header.jsx
@@ -1,38 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 import logo from "../../assets/img/logo-theater.png";
 import search_icon from "../../assets/img/search_icon.svg";
 import bell_icon from "../../assets/img/bell_icon.svg";
 import profile_img from "../../assets/img/profile_img.png";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (  
     <div className="navbar">
       <div className="navbar-left">
-        <Link to="/">
+        <Link to="/" onClick={closeMenu}>
           <img src={logo} alt="" />
         </Link>
-        <ul>
-          <ul>
-            <li>
-              <Link to="/">PHIM</Link>
-            </li>
-            <li>
-              <Link to="/rap-chieu-phim">RẠP CHIẾU PHIM</Link>
-            </li>
-            <li>
-              <Link to="/khuyen-mai">KHUYẾN MÃI</Link>
-            </li>
-            <li>
-              <Link to="/lien-he">LIÊN HỆ</Link>
-            </li>
-          </ul>
+        <div className="mobile-menu-toggle" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "active" : ""}>
+          <li>
+            <Link to="/" onClick={closeMenu}>PHIM</Link>
+          </li>
+          <li>
+            <Link to="/rap-chieu-phim" onClick={closeMenu}>RẠP CHIẾU PHIM</Link>
+          </li>
+          <li>
+            <Link to="/khuyen-mai" onClick={closeMenu}>KHUYẾN MÃI</Link>
+          </li>
+          <li>
+            <Link to="/lien-he" onClick={closeMenu}>LIÊN HỆ</Link>
+          </li>
         </ul>
       </div>
       <div className="navbar-right">
-        <Link to="/dat-ve" className="ticket-btn">ĐẶT VÉ NGAY
-        </Link>
-
+        <Link to="/dat-ve" className="ticket-btn" onClick={closeMenu}>ĐẶT VÉ NGAY</Link>
         <img src={search_icon} alt="" className="icons" />
         <img src={bell_icon} alt="" className="icons" />
         <div className="navbar-profile">
