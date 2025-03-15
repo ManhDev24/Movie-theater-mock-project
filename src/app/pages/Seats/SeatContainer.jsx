dd171faa49fa145c92c57c08c53c1fa6ed919878
NGUYEN QUANG MINH <quangminh190104@gmail.com>
2025-03-15 16:18:33 +0700

    Feat: Seat booking page, Refactor: MainRoutes.jsx-add SeatContainer

--- src/app/pages/Seats/SeatContainer.jsx
+++ src/app/pages/Seats/SeatContainer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import SeatsGrid from "./SeatsGrid";
-
+import { Outlet } from 'react-router-dom';
 const SeatContainer = () => {
   return (
+    <>    
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <SeatsGrid />
     </div>
+    <div className="bg-gray-200">
+      <Outlet/>
+    </div>
+
+    </>
   );
 };
 
